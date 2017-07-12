877764b31d459e268661e2ec4f19e04f2b20c1ee
Jerome Raffin <raffin.jerome@gmail.com>
2017-07-12 10:59:18 +0200

    create components and request

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import fetchGifs from '../utils/api';
+import Header from './Header';
+import GifsList from './GifsList';
+
+const StyledApp = styled.div`
+  height: 100%
+`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      gifs: []
+    }
+  }
+
+  handleChange = event => {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    fetchGifs(this.state.value).then(gifs => this.setState({gifs: gifs}))
+  }
+
   render() {
     return (
-      <div>Hello World!</div>
+      <StyledApp>
+        <Header />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Recherche:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        { this.state.gifs.length > 0 &&
+          <GifsList gifs={this.state.gifs} />
+        }
+      </StyledApp>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import 'normalize.css/normalize.css';
+import './global-styles';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
